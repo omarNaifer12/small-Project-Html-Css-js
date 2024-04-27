350995b03ca62a46834cf3717c05d1d729aef2a0
omarNaifer12 <o.naifer7@gmail.com>
2024-04-27 12:57:26 +0100

    done

--- Daily Drink Water Goal Project/main.js
+++ Daily Drink Water Goal Project/main.js
@@ -1,31 +1,50 @@
-var cups=document.getElementsByClassName("cup-small");
-var mainCup=document.getElementById("remained");
-var percentages=document.getElementById("percentage");
-var percentage=[12.5,25,37.5,50,62.5,75,87.5,100];
+var cups=document.querySelectorAll(".cup-small");
+var remained=document.getElementById("remained");
+var percentage=document.getElementById("percentage");
+
 var liters=document.getElementById("liters");
+var indexForUpdateHeight=-1;
 
-function editBackroundColor(){
 
-   var percentage=0;
-    return function progress(){
-        percentage++;
-        if(percentage<=12.5){
-            mainCup.style.height=percentage+"%";
-            mainCup.style.background="#00ffff"
-            setTimeout(progress,0)
+function updateIndex(idx){
+if(idx===indexForUpdateHeight){
+    indexForUpdateHeight--;
+    cups[idx].style.background="white";
+}
+else{
+
+indexForUpdateHeight=idx;
+updateBackroundCups(idx);
+}
+}
+function updateBackroundCups(idx){
+    for (let i = 0; i < cups.length; i++) {
+        if(i<=idx){
+            cups[i].style.background="blue";
         }
+        else{
+            cups[i].style.background="white"
+        }   
     }
+} 
+function updateHeight(){
+    if(indexForUpdateHeight===7){
+        remained.style.height=0;
+        remained.style.visibility='hidden'
+    }
+    else{
+        remained.style.visibility='visible';
+liters.innerText=`${0.25*(8-indexForUpdateHeight-1)}L`
+    }
+percentage.style.height=`${(330/8)*(indexForUpdateHeight+1)}px`
+if(indexForUpdateHeight!==-1)
+percentage.innerText=`${(indexForUpdateHeight+1)*100/8}%`
+else
+percentage.innerText="";
 }
-function editBackroundColors(percentage){
-    mainCup.style.height=percentage+"%";
-    mainCup.style.background="#00ffff"
-}
-var edit=editBackroundColor();
-setTimeout(edit,50)
-for (let i = 0; i < cups.length; i++) {
-    cups[i].addEventListener("click",function(event){
-        editBackroundColors(percentage[i]);
-        percentages.innerText=percentage[i].toString();
+cups.forEach(function(cup,idx){
+    cup.addEventListener('click',function(){
+updateIndex(idx);
+updateHeight();
     })
-}
-mmmkkk
+})
